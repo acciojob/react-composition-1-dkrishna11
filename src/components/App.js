7f296d08910c268bc80b3efa998299d830b79120
Krishna_D <dkrishna051997@gmail.com>
2023-07-16 23:03:50 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [tab, setTab]=useState("")
   return (
     <div>
-        {/* Do not remove the main div */}
+        <ul onClick={(eve)=>setTab(eve.target.value)}>
+          <li>Tab 1</li>
+          <li>Tab 3</li>
+          <li>Tab 3</li>
+        </ul>
+        <p>This is the content for {tab}</p>
     </div>
   )
 }
